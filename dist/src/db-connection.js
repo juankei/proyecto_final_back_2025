b7198f7e9bc7ab38d0b2d99603e0595501661bab
juankei <juancamilocosf@gmail.com>
2025-02-13 11:52:20 +0100

    me voy a pegar un tiro

--- dist/src/db-connection.js
+++ dist/src/db-connection.js
@@ -2,17 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.query = void 0;
 var pg_1 = require("pg");
-/*const pool = new Pool({
-  user: 'postgres',
-  password: '1234',
-  host: 'localhost',
-  port: 5432, // default Postgres port
-  database: 'proyecto'
-});*/
-var connectionString = 'postgresql://proyecto_final_24_25_lud5_user:UK3qFXkSC6TLblEIvGbLjeWUH2H509va@dpg-cum654ggph6c73dbu49g-a.frankfurt-postgres.render.com/proyecto_final_24_25_lud5';
 var pool = new pg_1.Pool({
-    connectionString: connectionString
+    user: 'postgres',
+    password: '1234',
+    host: 'localhost',
+    port: 5432,
+    database: 'proyecto'
 });
+/*
+const connectionString = 'postgresql://proyecto_final_24_25_lud5_user:UK3qFXkSC6TLblEIvGbLjeWUH2H509va@dpg-cum654ggph6c73dbu49g-a/proyecto_final_24_25_lud5'
+
+
+const pool = new Pool({
+  connectionString
+})*/
 function query(text) {
     return pool.query(text);
 }
--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,19 +1,19 @@
 import { Pool } from 'pg';
-
-/*const pool = new Pool({
+const pool = new Pool({
   user: 'postgres',
   password: '1234',
   host: 'localhost',
   port: 5432, // default Postgres port
   database: 'proyecto'
-});*/
+});
 
+/*
 const connectionString = 'postgresql://proyecto_final_24_25_lud5_user:UK3qFXkSC6TLblEIvGbLjeWUH2H509va@dpg-cum654ggph6c73dbu49g-a/proyecto_final_24_25_lud5'
 
 
 const pool = new Pool({
   connectionString
-})
+})*/
 
 export function query(text: any): any {
     return pool.query(text);
