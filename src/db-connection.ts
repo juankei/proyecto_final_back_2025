2ee868457f0ee83283097ab5a978efef81d50be2
juankei <juancamilocosf@gmail.com>
2025-02-13 09:33:24 +0100

    me quiero matar

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -8,7 +8,7 @@ import { Pool } from 'pg';
   database: 'proyecto'
 });*/
 
-const connectionString = 'postgresql://proyecto_final_24_25_lud5_user:UK3qFXkSC6TLblEIvGbLjeWUH2H509va@dpg-cum654ggph6c73dbu49g-a.frankfurt-postgres.render.com/proyecto_final_24_25_lud5'
+const connectionString = 'postgresql://proyecto_final_24_25_lud5_user:UK3qFXkSC6TLblEIvGbLjeWUH2H509va@dpg-cum654ggph6c73dbu49g-a/proyecto_final_24_25_lud5'
 
 
 const pool = new Pool({
