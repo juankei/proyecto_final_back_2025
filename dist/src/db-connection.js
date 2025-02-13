2329074297f37501b8076e1e89f07914bb48488f
juankei <juancamilocosf@gmail.com>
2025-02-13 12:54:36 +0100

    wdefwf

--- dist/src/db-connection.js
+++ dist/src/db-connection.js
@@ -2,20 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.query = void 0;
 var pg_1 = require("pg");
-var pool = new pg_1.Pool({
-    user: 'postgres',
-    password: '1234',
-    host: 'localhost',
-    port: 5432,
-    database: 'proyecto'
+/*const pool = new Pool({
+  user: 'postgres',
+  password: '1234',
+  host: 'localhost',
+  port: 5432, // default Postgres port
+  database: 'proyecto'
 });
-/*
-const connectionString = 'postgresql://proyecto_final_24_25_lud5_user:UK3qFXkSC6TLblEIvGbLjeWUH2H509va@dpg-cum654ggph6c73dbu49g-a/proyecto_final_24_25_lud5'
-
-
-const pool = new Pool({
-  connectionString
-})*/
+*/
+var connectionString = 'postgresql://proyecto_final_24_25_lud5_user:UK3qFXkSC6TLblEIvGbLjeWUH2H509va@dpg-cum654ggph6c73dbu49g-a/proyecto_final_24_25_lud5';
+var pool = new pg_1.Pool({
+    connectionString: connectionString
+});
 function query(text) {
     return pool.query(text);
 }
